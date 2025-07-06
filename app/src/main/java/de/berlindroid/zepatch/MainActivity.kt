a36bf085013291684d56120678daf655a54900d9
Louis <louis993546@gmail.com>
2025-07-06 11:44:23 +0200

    It's a list

--- app/src/main/java/de/berlindroid/zepatch/MainActivity.kt
+++ app/src/main/java/de/berlindroid/zepatch/MainActivity.kt
@@ -4,29 +4,77 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import de.berlindroid.zepatch.ui.theme.ZePatchTheme
 
+@ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ZePatchTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        LargeTopAppBar(title = { Text("ZePatch") })
+                    },
+                ) { innerPadding ->
                     LazyColumn(
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier.padding(innerPadding),
+                        verticalArrangement = Arrangement.spacedBy(8.dp),
                     ) {
                         items(
                             items = patchables.toList(),
                             key = { it.first },
-                        ) { (_, composable) ->
-                            composable()
+                        ) { (name, composable) ->
+                            Card(
+                                modifier = Modifier.fillMaxWidth(),
+                                onClick = { TODO("not implemented") },
+                            ) {
+                                Column(
+                                    modifier = Modifier.padding(8.dp),
+                                ) {
+                                    Row(
+                                        modifier = Modifier.fillMaxWidth(),
+                                        horizontalArrangement = Arrangement.SpaceBetween,
+                                    ) {
+                                        Text(name)
+                                        Icon(
+                                            imageVector = Icons.AutoMirrored.Filled.ArrowForward,
+                                            contentDescription = "TODO",
+                                        )
+                                    }
+                                    Box(
+                                        modifier = Modifier.border(
+                                            width = 1.dp,
+                                            color = Color.Black,
+                                        ),
+                                    ) {
+                                        composable()
+                                    }
+                                }
+                            }
                         }
                     }
                 }
