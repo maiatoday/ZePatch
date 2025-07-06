5a1d18f52b413ff912a31ac394fa33648b002e74
Louis <louis993546@gmail.com>
2025-07-06 15:14:41 +0200

    Edge-to-edge, list-detail, and more

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.parcelize)
 }
 
 android {
@@ -52,6 +53,9 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.adaptive)
+    implementation(libs.androidx.adaptive.layout)
+    implementation(libs.androidx.adaptive.navigation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/de/berlindroid/zepatch/MainActivity.kt
+++ app/src/main/java/de/berlindroid/zepatch/MainActivity.kt
@@ -9,9 +9,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsBottomHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -22,63 +25,126 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
+import androidx.compose.material3.adaptive.layout.AnimatedPane
+import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffoldRole
+import androidx.compose.material3.adaptive.navigation.NavigableListDetailPaneScaffold
+import androidx.compose.material3.adaptive.navigation.rememberListDetailPaneScaffoldNavigator
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 import de.berlindroid.zepatch.ui.theme.ZePatchTheme
+import kotlinx.coroutines.launch
 
 @ExperimentalMaterial3Api
+@ExperimentalMaterial3AdaptiveApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ZePatchTheme {
-                Scaffold(
-                    modifier = Modifier.fillMaxSize(),
-                    topBar = {
-                        LargeTopAppBar(title = { Text("ZePatch") })
-                    },
-                ) { innerPadding ->
-                    LazyColumn(
-                        modifier = Modifier.padding(innerPadding),
-                        verticalArrangement = Arrangement.spacedBy(8.dp),
-                    ) {
-                        items(
-                            items = patchables.toList(),
-                            key = { it.first },
-                        ) { (name, composable) ->
-                            Card(
-                                modifier = Modifier.fillMaxWidth(),
-                                onClick = { TODO("not implemented") },
-                            ) {
-                                Column(
-                                    modifier = Modifier.padding(8.dp),
+                val scaffoldNavigator = rememberListDetailPaneScaffoldNavigator()
+                val scope = rememberCoroutineScope()
+
+                NavigableListDetailPaneScaffold(
+                    navigator = scaffoldNavigator,
+                    listPane = {
+                        AnimatedPane {
+                            val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(
+                                rememberTopAppBarState()
+                            )
+                            Scaffold(
+                                modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+                                contentWindowInsets = WindowInsets.statusBars,
+                                topBar = {
+                                    LargeTopAppBar(
+                                        title = { Text("ZePatch") },
+                                        scrollBehavior = scrollBehavior,
+                                    )
+                                },
+                            ) { innerPadding ->
+                                PatchableList(
+                                    modifier = Modifier.padding(innerPadding),
                                 ) {
-                                    Row(
-                                        modifier = Modifier.fillMaxWidth(),
-                                        horizontalArrangement = Arrangement.SpaceBetween,
-                                    ) {
-                                        Text(name)
-                                        Icon(
-                                            imageVector = Icons.AutoMirrored.Filled.ArrowForward,
-                                            contentDescription = "TODO",
+                                    scope.launch {
+                                        scaffoldNavigator.navigateTo(
+                                            ListDetailPaneScaffoldRole.Detail,
+                                            it,
                                         )
                                     }
-                                    Box(
-                                        modifier = Modifier.border(
-                                            width = 1.dp,
-                                            color = Color.Black,
-                                        ),
+                                }
+                            }
+                        }
+                    },
+                    detailPane = {
+                        AnimatedPane {
+                            scaffoldNavigator.currentDestination?.contentKey?.let {
+                                // TODO: detail page, button to start "printing", preview
+                                Scaffold() { innerPadding ->
+                                    Column(
+                                        modifier = Modifier.padding(innerPadding),
                                     ) {
-                                        composable()
+                                        Text(it.toString())
                                     }
                                 }
                             }
                         }
                     }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun PatchableList(
+    modifier: Modifier = Modifier,
+    onItemClicked: (name: String) -> Unit,
+) {
+    LazyColumn(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        items(
+            items = patchables.toList(),
+            key = { it.first },
+        ) { (name, composable) ->
+            Card(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { onItemClicked(name) },
+            ) {
+                Column(
+                    modifier = Modifier.padding(8.dp),
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                    ) {
+                        Text(name)
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowForward,
+                            contentDescription = "TODO",
+                        )
+                    }
+                    Box(
+                        modifier = Modifier.border(
+                            width = 1.dp,
+                            color = Color.Black,
+                        ),
+                    ) {
+                        composable()
+                    }
                 }
             }
+        }
+        item(key = "navigationBar") {
+            Box(modifier = Modifier.windowInsetsBottomHeight(WindowInsets.navigationBars))
         }
     }
 }
