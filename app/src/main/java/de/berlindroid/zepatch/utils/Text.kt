ad0e2ac9503ec69ef9d3fa2803271567d8f748cc
Mario Bodemann <mario.bodemann@gmail.com>
2025-09-11 20:56:51 +0200

    polish: remove uppercase helper

--- app/src/main/java/de/berlindroid/zepatch/utils/Text.kt
+++
@@ -1,8 +0,0 @@
-package de.berlindroid.zepatch.utils
-
-fun String.uppercaseWords() =
-    replace("_", " ")
-        .split(" ")
-        .joinToString(separator = " ") {
-            it.first().uppercase() + it.drop(1).lowercase()
-        }
